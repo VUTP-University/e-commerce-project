9bebf38cbda2e6ae6c62ea8ccc472b52e084d587
torixfire <torixfire@git.co>
2025-12-06 13:12:13 +0200

    added address model

--- users-microservice/models.py
+++ users-microservice/models.py
@@ -10,6 +10,8 @@ class User(db.Model):
     password = db.Column(db.String(200), nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     
+    addresses = db.relationship('Address', back_populates='user', cascade='all, delete-orphan')
+
     
     def __init__(self, username, email, password):
         self.username = username
@@ -18,3 +20,23 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+class Address(db.Model):
+    __tablename__ = 'address'
+    id = db.Column(db.String(256), primary_key=True, default=lambda: str(uuid.uuid4()))
+    user_id = db.Column(db.String(256), db.ForeignKey('users.id'), nullable=False)
+    street = db.Column(db.String(80), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    zip = db.Column(db.String(10), nullable=False)
+    country = db.Column(db.String(10), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    
+    user = db.relationship('User', back_populates='addresses')
+
+    
+    def __init__(self, user_id, street, city, zip, country):
+        self.user_id = user_id
+        self.street = street
+        self.city = city
+        self.zip = zip
+        self.country = country
