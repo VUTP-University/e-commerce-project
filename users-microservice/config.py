f1e3909fed817c69652de4ef9facbe4e3c042f3c
torixfire <torixfire@git.co>
2025-12-12 19:53:44 +0200

    implemented addresses api

--- users-microservice/config.py
+++ users-microservice/config.py
@@ -1,9 +1,13 @@
 import os
+from datetime import timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class Config:
-    SECRET_KEY=os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    
+    # JWT Configuration
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
